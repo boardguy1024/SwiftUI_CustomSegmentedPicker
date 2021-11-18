f4fb2df165a7976a9a7fe068cb80cee9ea7f6a69
boardguy1024 <boardguy1024@gmail.com>
2021-11-18 18:39:59 +0900

    first commit

---
+++ SwiftUI_CustomSegmentedPicker/SwiftUI_CustomSegmentedPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CustomSegmentedPickerApp.swift
+//  SwiftUI_CustomSegmentedPicker
+//
+//  Created by park kyung seok on 2021/11/18.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CustomSegmentedPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
